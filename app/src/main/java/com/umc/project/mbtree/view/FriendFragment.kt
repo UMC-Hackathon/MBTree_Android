6cb2de480c2a5f4279d3c140ae9eb8365aa9bb91
eun <eunmoong@gmail.com>
2022-08-24 03:39:22 +0900

    버전카톡친구

--- app/src/main/java/com/umc/project/mbtree/view/FriendFragment.kt
+++ app/src/main/java/com/umc/project/mbtree/view/FriendFragment.kt
@@ -1,11 +1,13 @@
 package com.umc.project.mbtree.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.kakao.sdk.talk.TalkApiClient
 import com.umc.project.mbtree.data.User
 import com.umc.project.mbtree.databinding.FragmentFriendBinding
 import com.umc.project.mbtree.view.friend.FriendRVAdapter
@@ -28,8 +30,20 @@ class FriendFragment: Fragment() {
             add(User(2, "user2", "234", "ISFJ"))
             add(User(3, "user3", "345", "ESFP"))
         }
+        TalkApiClient.instance.friends { friends, error ->
+            if (error != null) {
+                Log.e("실패", "카카오톡 친구 목록 가져오기 실패", error)
+            }
+            else if (friends != null) {
+                Log.i("성공", "카카오톡 친구 목록 가져오기 성공 \n${friends.elements?.joinToString("\n")}")
 
-        //채팅리스트 어댑터 붙이기
+                // 친구의 UUID 로 메시지 보내기 가능
+            }
+
+        }
+
+
+        //친구리스트 어댑터 붙이기
         val fAdaper = FriendRVAdapter(datas)
         binding.rvFriendList.adapter = fAdaper
         binding.rvFriendList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
