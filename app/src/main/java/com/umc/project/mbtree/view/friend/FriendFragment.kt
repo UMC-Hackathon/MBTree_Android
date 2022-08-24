866860eea4e87c3b1f8ddb1fcea7ad264041c12d
eun <eunmoong@gmail.com>
2022-08-24 15:09:10 +0900

    카카오로그인안뜸

--- app/src/main/java/com/umc/project/mbtree/view/friend/FriendFragment.kt
+++ app/src/main/java/com/umc/project/mbtree/view/friend/FriendFragment.kt
@@ -1,16 +1,19 @@
 package com.umc.project.mbtree.view.friend
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.kakao.sdk.talk.TalkApiClient
 import com.umc.project.mbtree.R
 import com.umc.project.mbtree.data.User
 import com.umc.project.mbtree.databinding.FragmentFriendBinding
 import com.umc.project.mbtree.view.LetterPaper.FriendTreeFragment
 import com.umc.project.mbtree.view.MainActivity
+import kotlinx.android.synthetic.main.activity_after_login.*
 
 class FriendFragment: Fragment() {
 
@@ -31,7 +34,17 @@ class FriendFragment: Fragment() {
             add(User(3, "user3", "345", "ESFP"))
         }
 
+        kakao_friends_button.setOnClickListener {
+            TalkApiClient.instance.friends { friends, error ->
+                if (error != null) {
+                    Log.e("실패", "카카오톡 친구 목록 가져오기 실패", error)
+                } else if (friends != null) {
+                    Log.i("성공", "카카오톡 친구 목록 가져오기 성공 \n${friends.elements?.joinToString("\n")}")
 
+                    // 친구의 UUID 로 메시지 보내기 가능
+                }
+            }
+        }
         //채팅리스트 어댑터 붙이기
         val fAdaper = FriendRVAdapter(datas)
         binding.rvFriendList.adapter = fAdaper
